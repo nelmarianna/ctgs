eead844da25f9ea65ed8e0d304b0e12f0ecfd735
nelmarianna <aniela.opolski@gmail.com>
2016-12-05 23:44:46 -0500

    notices

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,14 +27,14 @@ class UsersController < ApplicationController
 
 		 if (@type=="Requester") && (@user.sup_id).present? && (@user.firstName).present? && (@user.lastName).present?&& (@user.loginID).present? && (@user.password).present?&& (@user.email).present? && (@user.academicUnit).present? && (@user.studNumb).present? && (@user.program).present? && (@user.sessionNumber).present? && (@user.thesisTopic).present? && (@user.bankAccNumb).present?
 			@user.save
-		# 	      flash[:notice] = "Country created successfully"
+			flash[:success] = "Requester created successfully"
 		redirect_to "/home"
 		 elsif((@type=="Supervisor") && (@user.firstName).present? && (@user.lastName).present?)
 			@user.save
 		 	redirect_to "/home"
+		 	flash[:success] = "Supervisor successfully created"
 		 else 
-		# 	      flash[:notice] = "CHAHAHAHAHlly"
-
+			flash[:error] = "User not created"
 			render("new")
 		 end
 	#	Rails.logger.debug(@type)
